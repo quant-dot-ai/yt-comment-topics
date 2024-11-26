030f3b50870f93cf2946ab2743ea8d8b53a9e104
Pritesh Singh <146693417+quant-dot-ai@users.noreply.github.com>
2024-11-26 18:08:03 -0500

    Update homepage.py
    - testing no top_n parameter in fasTopic

--- homepage.py
+++ homepage.py
@@ -44,7 +44,7 @@ def get_comments(video_id, next_page_token=None):
 
 def get_topics_from_fasTopic(comments_text):
     preprocessing = Preprocessing(stopwords='English')
-    model = FASTopic(num_topics=5, preprocessing=preprocessing)
+    model = FASTopic( preprocessing=preprocessing)
     topic_top_words, doc_topic_dist = model.fit_transform(comments_text)
     # st.table(topic_top_words)
 
