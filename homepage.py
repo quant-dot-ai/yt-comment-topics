2e60ba3ea38f702d281020d1bcdf578bd06ca4cd
Pritesh Singh <146693417+quant-dot-ai@users.noreply.github.com>
2024-11-26 18:09:22 -0500

    Update homepage.py
    - Using transform instead of fit_transform

--- homepage.py
+++ homepage.py
@@ -44,8 +44,8 @@ def get_comments(video_id, next_page_token=None):
 
 def get_topics_from_fasTopic(comments_text):
     preprocessing = Preprocessing(stopwords='English')
-    model = FASTopic( preprocessing=preprocessing)
-    topic_top_words, doc_topic_dist = model.fit_transform(comments_text)
+    model = FASTopic(num_topics=5, preprocessing=preprocessing)
+    topic_top_words, doc_topic_dist = model.transform(comments_text)
     # st.table(topic_top_words)
 
     # Display topic words in table form
