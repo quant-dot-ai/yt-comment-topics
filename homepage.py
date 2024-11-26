22b55460a12f7db59245b4b51aa49b20d34109ef
Pritesh Singh <146693417+quant-dot-ai@users.noreply.github.com>
2024-11-26 15:49:13 -0500

    Update homepage.py
    added call to get comments

--- homepage.py
+++ homepage.py
@@ -19,40 +19,39 @@ video_id = st.text_input("Enter YouTube Video ID", "")
 if st.button("Analyze Topics"):
     if video_id.strip():
         st.info("Fetching comments...")
-        st.text(api_key)
         # comments = fetch_comments(video_id)
-        # get_comments(video_id)
+        get_comments(video_id)
         # st.dataframe(df_with_n_relevant_comments)
 
 df_with_n_relevant_comments = []
 
-# def get_comments(video_id, next_page_token=None):
-#     comments = []
+def get_comments(video_id, next_page_token=None):
+    comments = []
     
-#     request = youtube.commentThreads().list(
-#         part="snippet",
-#         videoId=video_id,
-#         textFormat="plainText",
-#         maxResults=10,
-#         pageToken=next_page_token,
-#         order="relevance"
-#     )
-#     response = request.execute()
+    request = youtube.commentThreads().list(
+        part="snippet",
+        videoId=video_id,
+        textFormat="plainText",
+        maxResults=10,
+        pageToken=next_page_token,
+        order="relevance"
+    )
+    response = request.execute()
 
-#     for item in response["items"]:
-#         comment = item["snippet"]["topLevelComment"]["snippet"]
-#         comments.append([
-#             comment["authorDisplayName"],
-#             comment["publishedAt"],
-#             comment["updatedAt"],
-#             comment["likeCount"],
-#             comment["textDisplay"]
-#         ])
+    for item in response["items"]:
+        comment = item["snippet"]["topLevelComment"]["snippet"]
+        comments.append([
+            comment["authorDisplayName"],
+            comment["publishedAt"],
+            comment["updatedAt"],
+            comment["likeCount"],
+            comment["textDisplay"]
+        ])
     
-#     if "nextPageToken" in response:
-#         get_comments(video_id, response["nextPageToken"])
+    if "nextPageToken" in response:
+        get_comments(video_id, response["nextPageToken"])
 
-#     df_with_n_relevant_comments=pd.DataFrame(comments, columns=["author", "published_at", "updated_at", "like_count", "text"])
-#     st.dataframe(df_with_n_relevant_comments)
+    df_with_n_relevant_comments=pd.DataFrame(comments, columns=["author", "published_at", "updated_at", "like_count", "text"])
+    st.dataframe(df_with_n_relevant_comments)
 
-#     # return df_with_n_relevant_comments
+    # return df_with_n_relevant_comments
